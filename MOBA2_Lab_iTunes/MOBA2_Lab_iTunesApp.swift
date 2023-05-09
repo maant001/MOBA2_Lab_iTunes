7018606b42ba89f6c3651ddab81fc280eb9f66c7
maant001 <mamaril_a@me.com>
2023-05-09 20:17:37 +0200

    Initial Commit

---
+++ MOBA2_Lab_iTunes/MOBA2_Lab_iTunesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOBA2_Lab_iTunesApp.swift
+//  MOBA2_Lab_iTunes
+//
+//  Created by Tony Mamaril on 09.05.23.
+//
+
+import SwiftUI
+
+@main
+struct MOBA2_Lab_iTunesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
